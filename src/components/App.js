9a3d700ea6f9c23ee7ff27d6a4de09bf419d077d
jhony maurad <jhonymaurad@gmail.com>
2019-07-21 19:09:12 -0400

    Generated inital app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+    render(){
+        return(
+            <div className="ui container">hello world</div>
+        )
+    }
+    
+}
+
+export default App;
